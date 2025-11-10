284a350ef98e4beee12f5ef184143487077b79cd
Neo Yuki Aylor <neoyukiaylor@gmail.com>
2025-11-10 05:16:42 +0000

    added geode version number to version display

--- changelog.md
+++ changelog.md
@@ -1,16 +1,23 @@
-# v1.0.2
-- Initial Release
+# v1.1.0
+- Added geode version number to the version display
+
+# v1.0.7
+- Dropped support for apple devices
+  - I no longer have a reliable way to test the mod for these platforms
+
+# v1.0.6
+- Improved the v + versionCode concat code
+
+# v1.0.5
+- Fixed 2 back to back instances of the word "the" in README.md
+
+# v1.0.4
+- Forgot to update changelog.md
 
 # v1.0.3
 - Added links to the homepage and source, both of which point to this repo.
 - Added a support link because geode doesn't actually disable the support button when the support.md file is missing.
   - As a result I may as well put my PayPal link there, I guess.
 
-# v1.0.4
-- Forgot to update changelog.md
-
-# v1.0.5
-- Fixed 2 back to back instances of the word "the" in README.md
-
-# v1.0.6
-- Improved the v + versionCode concat code
+# v1.0.2
+- Initial Release
--- src/main.cpp
+++ src/main.cpp
@@ -4,52 +4,54 @@
 using namespace geode::prelude;
 
 class $modify(MenuLayer) {
-    bool init() {
-        if (!MenuLayer::init()) return false;
+  bool init() {
+    if (!MenuLayer::init()) return false;
 
-		auto confPos = Mod::get()->getSettingValue<std::string>("version-position");
-		auto confOpacity = Mod::get()->getSettingValue<double>("version-opacity");
-		auto confDebug = Mod::get()->getSettingValue<bool>("debug-enabled");
-		
-		// Got some help in DMs with this, cpp really isnt that intuitive to me.
-		auto curGDVer = "v" + Loader::get()->getGameVersion();
-		auto verLabel = CCLabelBMFont::create(curGDVer.c_str(), "bigFont.fnt");
-		
-		auto menuHeight = getContentHeight();
-		auto menuWidth = getContentWidth();
+    auto confPos = Mod::get()->getSettingValue<std::string>("version-position");
+    auto confOpacity = Mod::get()->getSettingValue<double>("version-opacity");
+    auto confDebug = Mod::get()->getSettingValue<bool>("debug-enabled");
 
-		// Here for making sure I actually got what values I wanted
-		if (confDebug == true) {
-			log::debug("Position: {}", confPos);
-			log::debug("Main Menu Height: {}", menuHeight);
-			log::debug("Main Menu Width: {}", menuWidth);
-			log::debug("Opacity: {}", confOpacity);
-			log::debug("Game Version: {}", curGDVer);
-		}
+    // Got some help in DMs with this, cpp really isnt that intuitive to me.
+    auto curGDVer = Loader::get()->getGameVersion();
+    auto curGeodeVer = Loader::get()->getVersion().toNonVString();
+    auto labelString = "GD: v" + curGDVer + " (Geode: v" + curGeodeVer + ")";
+    auto labelItem = CCLabelBMFont::create(labelString.c_str(), "bigFont.fnt");
 
-		// There is probably a better way of implementing this but it works and doesnt look that bad (at least to me)
-		if (confPos == "Top Left") {
-			verLabel->setPosition(0, menuHeight);
-			verLabel->setAnchorPoint({-0.05f, 1.05f});
-		}
-		if (confPos == "Top Right") {
-			verLabel->setPosition(menuWidth, menuHeight);
-			verLabel->setAnchorPoint({1.05f, 1.05f});
-		}
-		if (confPos == "Bottom Left") {
-			verLabel->setPosition(0, 0);
-			verLabel->setAnchorPoint({-0.05f, -0.2f});
-		}
-		if (confPos == "Bottom Right") {
-			verLabel->setPosition(menuWidth, 0);
-			verLabel->setAnchorPoint({1.05f, -0.2f});	
-		}
+    auto menuHeight = getContentHeight();
+    auto menuWidth = getContentWidth();
 
-		verLabel->setScale(0.25f);
-		verLabel->setOpacity(confOpacity);
-		verLabel->setID("game-version-label");
-        this->addChild(verLabel);
-
-        return true;
+    // Here for making sure I actually got what values I wanted
+    if (confDebug == true) {
+      log::info("Position: {}", confPos);
+      log::info("Main Menu Height: {}", menuHeight);
+      log::info("Main Menu Width: {}", menuWidth);
+      log::info("Opacity: {}", confOpacity);
+      log::info("Game Version: {}", curGDVer);
+      log::info("Geode Version: {}", curGeodeVer);
     }
+
+    // There is probably a better way of implementing this but it works and doesnt look that bad (at least to me)
+    if (confPos == "Top Left") {
+      labelItem->setPosition(0, menuHeight);
+      labelItem->setAnchorPoint({-0.05f, 1.05f});
+    }
+    if (confPos == "Top Right") {
+      labelItem->setPosition(menuWidth, menuHeight);
+      labelItem->setAnchorPoint({1.05f, 1.05f});
+    }
+    if (confPos == "Bottom Left") {
+      labelItem->setPosition(0, 0);
+      labelItem->setAnchorPoint({-0.05f, -0.2f});
+    }
+    if (confPos == "Bottom Right") {
+      labelItem->setPosition(menuWidth, 0);
+      labelItem->setAnchorPoint({1.05f, -0.2f});
+    }
+
+    labelItem->setScale(0.25f);
+    labelItem->setOpacity(confOpacity);
+    labelItem->setID("game-version-label");
+    this->addChild(labelItem);
+    return true;
+  }
 };
